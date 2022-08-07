aa4d367e55bec429e4994e52e67385cb30fcf396
Lloyd Prock <lloyd.prock@gmail.com>
2022-08-06 23:16:09 -0500

    Working operators and calculate

--- app.js
+++ app.js
@@ -11,11 +11,46 @@ function updateDisplay() {
 }
 
 function keyPress(key) {
-    let { displayValue } = calculator;
-    if (displayValue === "0") {
+    const { displayValue } = calculator;
+    if (calculator.waitingSecondOperand === true) {
+        calculator.displayValue = key;
+        calculator.waitingSecondOperand = false;
+    } else if (displayValue === "0") {
         calculator.displayValue = key;
     } else {
         calculator.displayValue = displayValue + key;
+    }
+    console.log(calculator);
+}
+
+function keyOperatorPress(key) {
+    let { displayValue, operator, firstOperand } = calculator;
+    const curValue = parseFloat(displayValue);
+    if (firstOperand === null && !isNaN(curValue)) {
+        calculator.firstOperand = curValue;
+    } else if (operator) {
+        const total = calculate(firstOperand, displayValue, operator);
+        calculator.displayValue = String(total);
+        calculator.firstOperand = total;
+    }
+    calculator.operator = key;
+    calculator.waitingSecondOperand = true;
+    console.log(calculator);
+}
+
+function calculate(first, second, operator) {
+    switch (operator) {
+        case "+":
+            return first + second;
+        case "-":
+            return first - second;
+        case "*":
+            return first * second;
+        case "/":
+            return first / second;
+        default:
+            return second;
+
     }
 }
 
@@ -26,19 +61,20 @@ keys.addEventListener("click", (e) => {
         return;
     }
     if (target.classList.contains("operatorKeys")) {
-        console.log("operator", target.value)
+        keyOperatorPress(target.value);
+        console.log("operator", target.value);
         return;
     }
     if (target.classList.contains("decimalKey")) {
-        console.log("decimal", target.value)
+        console.log("decimal", target.value);
         return;
     }
     if (target.classList.contains("clearKey")) {
-        console.log("all clear", target.value)
+        console.log("all clear", target.value);
         return;
     }
     if (target.classList.contains("equalKey")) {
-        console.log("equals", target.value)
+        console.log("equals", target.value);
         return;
     }
     keyPress(target.value);
