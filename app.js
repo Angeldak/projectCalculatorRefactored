3053a4ddaeadee419b5bb07b5c4c38f49a8f0145
Lloyd Prock <lloyd.prock@gmail.com>
2022-08-11 22:31:21 -0500

    fixed repeating decimal and set max digits to 11

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let maxLength = 8;
+
 const calculator = {
     displayValue: "0",
     operator: null,
@@ -5,19 +7,16 @@ const calculator = {
     waitingSecondOperand: false
 }
 
-//NEED TO ADD functionality for trueValue to show on HTML with full digit display
 function updateDisplay() {
     const display = document.querySelector(".calcDisplay");
     const excessWrapper = document.querySelector("#excessWrapper");
-    const trueValue = document.querySelector("#trueValue");
-    display.value = calculator.displayValue;
-
-    if (calculator.displayValue.length > 10) {
+    if (calculator.displayValue.length > 11) {
         excessWrapper.classList.remove("excessWrapperOff");
         excessWrapper.classList.add("excessWrapperOn");
     } else {
         excessWrapper.classList.add("excessWrapperOff");
         excessWrapper.classList.remove("excessWrapperOn");
+        display.value = calculator.displayValue;
     }
 }
 
@@ -37,15 +36,14 @@ function keyOperatorPress(key) {
     let { displayValue, operator, firstOperand } = calculator;
     const curValue = parseFloat(displayValue);
     if (firstOperand === null && !isNaN(curValue)) {
-        calculator.firstOperand = curValue;
+        calculator.firstOperand = curValue.toFixed(maxLength);
     } else if (operator) {
         const total = calculate(firstOperand, curValue, operator);
-        calculator.displayValue = String(total);
+        calculator.displayValue = String(total.toFixed(maxLength));
         calculator.firstOperand = total;
     }
     calculator.operator = key;
     calculator.waitingSecondOperand = true;
-    console.log(calculator);
 }
 
 function calculate(first, second, operator) {
@@ -74,7 +72,8 @@ function totalOut() {
     const { operator, firstOperand, displayValue } = calculator;
     const curValue = parseFloat(displayValue);
     if (operator) {
-        calculator.displayValue = calculate(firstOperand, curValue, operator);
+        let newNum = calculate(firstOperand, curValue, operator);
+        calculator.displayValue = newNum.toFixed(maxLength).toString();
         calculator.waitingSecondOperand = false;
         calculator.firstOperand = null;
         calculator.operator = null;
